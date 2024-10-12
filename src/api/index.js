7b0c7832010bbb9013517fa61c22852673785419
owais <owaisahmed142002@gmail.com>
2024-10-12 20:18:35 +0500

    few changes

--- src/api/index.js
+++ src/api/index.js
@@ -2,25 +2,25 @@ import axios from "axios";
 import { API_BASE_URL } from "../apiConfig"; // Import the centralized URL
 
 export const fetchNamespaces = async () => {
-  return axios.get(`${API_BASE_URL}/chatbot/lawyer/fetch-namespaces/`);
+  return axios.get(`${API_BASE_URL}/chatbot/fetch-namespaces/`);
 };
 
 export const fetchNamespacesData = async (country, page) => {
-  return axios.post(`${API_BASE_URL}/chatbot/lawyer/fetch-namespaces-data/`, {
+  return axios.post(`${API_BASE_URL}/chatbot/fetch-namespaces-data/`, {
     country,
     page,
   });
 };
 
 export const deleteEmbedding = async (country, link) => {
-  return axios.post(`${API_BASE_URL}/chatbot/lawyer/delete-embedding/`, {
+  return axios.post(`${API_BASE_URL}/chatbot/delete-embedding/`, {
     country,
     link,
   });
 };
 
 export const embedDocument = async (fileURL, country, category, metadata) => {
-  return axios.post(`${API_BASE_URL}/chatbot/lawyer/embed/`, {
+  return axios.post(`${API_BASE_URL}/chatbot/embed/`, {
     fileURL,
     country,
     category,
@@ -29,27 +29,27 @@ export const embedDocument = async (fileURL, country, category, metadata) => {
 };
 
 export const createNewCountry = async (country) => {
-  return axios.post(`${API_BASE_URL}/chatbot/lawyer/create-new-country/`, {
+  return axios.post(`${API_BASE_URL}/chatbot/create-new-country/`, {
     country,
   });
 };
 
 export const deleteCountry = async (country) => {
-  return axios.post(`${API_BASE_URL}/chatbot/lawyer/delete-country/`, { country });
+  return axios.post(`${API_BASE_URL}/chatbot/delete-country/`, { country });
 };
 
 export const getUniqueLinks = async (country) => {
-  return axios.post(`${API_BASE_URL}/chatbot/lawyer/get-unique-links/`, {
+  return axios.post(`${API_BASE_URL}/chatbot/get-unique-links/`, {
     country,
   });
 };
 
 export const getSystemMessage = async () => {
-  return axios.get(`${API_BASE_URL}/chatbot/lawyer/system-message/`);
+  return axios.get(`${API_BASE_URL}/chatbot/system-message/`);
 };
 
 export const updateSystemMessage = async (system_message) => {
-  return axios.post(`${API_BASE_URL}/chatbot/lawyer/system-message/`, {
+  return axios.post(`${API_BASE_URL}/chatbot/system-message/`, {
     system_message,
   });
 };
